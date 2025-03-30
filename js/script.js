0dbac53769e899bebb5866f9b9af2fba2e360e3c
Yanis2002 <70938875+Yanis2002@users.noreply.github.com>
2025-03-30 16:39:37 +0300

    Fix theme and language switchers

--- js/script.js
+++ js/script.js
@@ -1,32 +1,35 @@
-var checkbox = document.querySelector('.theme-switch__checkbox');
-
-checkbox.addEventListener('change', function(){
-  transition();
-  if (this.checked) {
-    document.documentElement.setAttribute('data-theme', 'dark');
-  } else {
-    document.documentElement.setAttribute('data-theme', 'light');
-  }
-})
-
-function transition() {
-  document.documentElement.classList.add('transition');
-  setTimeout(function() {
-    document.documentElement.classList.remove('transition');
-  }, 250)
-}
+// Theme switcher
+const themeSwitch = document.querySelector('.theme-switch__checkbox');
+const body = document.body;
 
 // Language switcher
 const languageSwitch = document.querySelector('.language-switch__checkbox');
 const ruElements = document.querySelectorAll('.ru');
 const enElements = document.querySelectorAll('.en');
 
+// Theme switcher functionality
+themeSwitch.addEventListener('change', function() {
+  transition();
+  if (this.checked) {
+    document.documentElement.setAttribute('data-theme', 'dark');
+  } else {
+    document.documentElement.setAttribute('data-theme', 'light');
+  }
+});
+
 // Language switcher functionality
-languageSwitch.addEventListener('change', () => {
+languageSwitch.addEventListener('change', function() {
   ruElements.forEach(element => {
-    element.style.display = languageSwitch.checked ? 'none' : 'block';
+    element.style.display = this.checked ? 'none' : 'block';
   });
   enElements.forEach(element => {
-    element.style.display = languageSwitch.checked ? 'block' : 'none';
+    element.style.display = this.checked ? 'block' : 'none';
   });
 });
+
+function transition() {
+  document.documentElement.classList.add('transition');
+  setTimeout(function() {
+    document.documentElement.classList.remove('transition');
+  }, 250);
+}
